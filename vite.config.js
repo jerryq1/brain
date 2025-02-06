077dd4ae233ece2add4f04f4a93c9ca4a4ade68f
jerry <xuzhanhong@gialen.com>
2025-02-06 10:57:01 +0800

    all

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  base: process.env.NODE_ENV === 'production'
+    ? '/brain/' // 例如 '/my-vue3-demo/'
+    : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
